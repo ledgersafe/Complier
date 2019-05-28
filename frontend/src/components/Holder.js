afc3a8a6db3a827bab715e177cebf0a86e668a6f
Danny Fung <danny@cryptos-MacBook-Pro.local>
2019-05-28 11:21:21 -0700

    more notifications

--- frontend/src/components/Holder.js
+++ frontend/src/components/Holder.js
@@ -29,32 +29,40 @@ class Product extends Component {
 
     sellAssets(e) {
         e.preventDefault();
-        ReactDOM.findDOMNode(this.refs.sold).innerHTML = "<p>Selling Assets, please wait...</p>";
         ReactDOM.findDOMNode(this.refs.sold).style.height = "50px";
+        if(!this.id || !this.holder){
+        ReactDOM.findDOMNode(this.refs.sold).innerHTML = "<p>Please fill in all fields.</p>";
         ReactDOM.findDOMNode(this.refs.sold).style.color = "#7a7a7a";
-        $.ajax({
-            url: 'http://localhost:4000/change',
-            type: 'POST',
-            contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
-            crossDomain: true,
-            dataType: 'json',
-            xhrFields: { withCredentials: true },
-            data: {
-                id: this.id,
-                holder: this.holder
-            },
-            success: (data) => {
-                if (data.message === 'OK') {
-                    console.log('change_holder success!')
-                    ReactDOM.findDOMNode(this.refs.sold).innerHTML = "<p>Assets sold! Transaction ID: "+data.tx_id+"</p>";
-                    ReactDOM.findDOMNode(this.refs.sold).style.color = "#acd854";
-                    this.getAll()
+        }
+        else{
+            ReactDOM.findDOMNode(this.refs.sold).innerHTML = "<p>Selling Assets, please wait...</p>";
+            ReactDOM.findDOMNode(this.refs.sold).style.color = "#7a7a7a";
+            $.ajax({
+                url: 'http://localhost:4000/change',
+                type: 'POST',
+                contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+                crossDomain: true,
+                dataType: 'json',
+                xhrFields: { withCredentials: true },
+                data: {
+                    id: this.id,
+                    holder: this.holder
+                },
+                success: (data) => {
+                    if (data.message === 'OK') {
+                        console.log('change_holder success!')
+                        ReactDOM.findDOMNode(this.refs.sold).innerHTML = "<p>Assets sold! Transaction ID: "+data.tx_id+"</p>";
+                        ReactDOM.findDOMNode(this.refs.sold).style.color = "#acd854";
+                        this.getAll()
+                    }
+                    else {
+                        ReactDOM.findDOMNode(this.refs.sold).innerHTML = "<p>An error has occurred.</p>";
+                        ReactDOM.findDOMNode(this.refs.sold).style.color = "#7a7a7a";
+                        console.log('ERROR');
+                    }
                 }
-                else {
-                    console.log('ERROR');
-                }
-            }
-        });
+            });
+        }
     }
 
     render() {
