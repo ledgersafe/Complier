fe3a313c4bc138c0c1d378201155570967e7d71c
kmiyachi <kmiyachi@ucsd.edu>
2019-09-04 21:02:11 -0700

    a new hope

--- backend/express.js
+++ backend/express.js
@@ -17,8 +17,67 @@ var peer = fabric_client.newPeer("grpc://localhost:7051");
 channel.addPeer(peer);
 
 // Helper Functions
-const routerTo_queryAll = require('./queryAll.js')()
+// const routerTo_queryAll = require('./queryAll.js')()
 var sell = require('./b2bTransaction.js')
+
+var queryAll = async function (req, res) {
+    try {
+        console.log("getting all cannabis from database: ");
+
+        //
+        var member_user = null;
+        var store_path = path.join(os.homedir(), ".hfc-key-store");
+        console.log("Store path:" + store_path);
+        var tx_id = null;
+
+        const state_store = await Fabric_Client.newDefaultKeyValueStore({ path: store_path });
+        // assign the store to the fabric client
+        fabric_client.setStateStore(state_store);
+        var crypto_suite = Fabric_Client.newCryptoSuite();
+        // use the same location for the state store (where the users' certificate are kept)
+        // and the crypto store (where the users' keys are kept)
+        var crypto_store = Fabric_Client.newCryptoKeyStore({
+            path: store_path
+        });
+        crypto_suite.setCryptoKeyStore(crypto_store);
+        fabric_client.setCryptoSuite(crypto_suite);
+
+        const user_from_store = await fabric_client.getUserContext("user1", true);
+        if (user_from_store && user_from_store.isEnrolled()) {
+            console.log("Successfully loaded user1 from persistence");
+            member_user = user_from_store;
+        } else {
+            throw new Error("Failed to get user1.... run registerUser.js");
+        }
+
+        // queryAllCannabis - requires no arguments , ex: args: [''],
+        const request = {
+            chaincodeId: "ledgersafe-app",
+            txId: tx_id,
+            fcn: "queryAllCannabis",
+            args: [""]
+        };
+
+        const query_responses = await channel.queryByChaincode(request);
+        console.log("Query has completed, checking results in queryAll.js");
+        // query_responses could have more than one  results if there multiple peers were used as targets
+        if (query_responses && query_responses.length == 1) {
+            if (query_responses[0] instanceof Error) {
+                console.error("error from query = ", query_responses[0]);
+            } else {
+                console.log("Response is ", query_responses[0].toString());
+                // res.json(JSON.parse(query_responses[0].toString()));
+                return JSON.parse(query_responses[0].toString());
+            }
+        } else {
+            console.log("No payloads were returned from query");
+        }
+    }
+    catch (error) {
+        console.error('query failed, ', error);
+        process.exit(1);
+    }
+}
 
 var add_cannabis = async function (req, res) {
     try {
@@ -528,7 +587,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/queryAll', routerTo_queryAll);
+// app.use('/queryAll', routerTo_queryAll);
 
 app.use('/add', function (req, res) {
     add_cannabis(req, res);
@@ -563,6 +622,16 @@ app.use('/getHistory', function (req, res) {
             res.status(200).json({ message: 'OK', history: history })
         } else {
             res.status(200).json({ message: 'NOK', history: history })
+        }
+    });
+});
+
+app.use('/queryAll', function(req, res) {
+    queryAll(req, res).then(function (result) {
+        if (result) {
+            res.status(200).json({ message: 'OK', result: result })
+        } else {
+            res.status(200).json({ message: 'NOK', result: result })
         }
     });
 });
